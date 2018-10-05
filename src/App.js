36303209124b8f4d2a09e976aeabc800e7c3ac1d
tboychuk <tarass.boychuk@gmail.com>
2018-10-05 18:27:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,16 +16,11 @@ class App extends Component {
             <div className="App">
                 <Map
                     style="mapbox://styles/boy4uck/cj8795w2h3c5k2qs6sq35cu9z"
+                    center={[24.49891090393066, 48.36674224855921]}
                     containerStyle={{
                         height: "100vh",
                         width: "100vw"
                     }}>
-                    <Layer
-                        type="symbol"
-                        id="marker"
-                        layout={{"icon-image": "marker-15"}}>
-                        <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
-                    </Layer>
                 </Map>
 
             </div>
