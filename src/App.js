32bedac128c759733331f78c226ef51dcab0c42f
Vlad Kondratenko <omgkanamikun@gmail.com>
2018-10-05 16:22:14 +0300

    issue#1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+// ES6
+import ReactMapboxGl, {Layer, Feature} from "react-mapbox-gl";
+
+const Map = ReactMapboxGl({
+    accessToken: "pk.eyJ1IjoiYm95NHVjayIsImEiOiJkWlgxdVhRIn0.nzy-n4qIL6QNPiWtTO_UTw"
+});
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            <h1>Welcome to Bobocode</h1>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+
+    render() {
+        return (
+            <div className="App">
+                <Map
+                    style="mapbox://styles/boy4uck/cj8795w2h3c5k2qs6sq35cu9z"
+                    containerStyle={{
+                        height: "100vh",
+                        width: "100vw"
+                    }}>
+                    <Layer
+                        type="symbol"
+                        id="marker"
+                        layout={{"icon-image": "marker-15"}}>
+                        <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+                    </Layer>
+                </Map>
+
+            </div>
+        );
+    }
 }
 
 export default App;
